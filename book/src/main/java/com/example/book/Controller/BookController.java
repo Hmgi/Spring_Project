95ebdf1555e0999bc31ccdc2466649e5e91540b8
Hmgi <hk335078@naver.com>
2022-01-17 18:39:41 +0900

    add controller

--- book/src/main/java/com/example/book/Controller/BookController.java
+++ book/src/main/java/com/example/book/Controller/BookController.java
@@ -1,12 +1,33 @@
 package com.example.book.Controller;
 
+import com.example.book.domain.Book;
+import com.example.book.repository.BookRepository;
+import com.example.book.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class BookController {
-    @GetMapping("create")   // -> localhost:8080/create 접속하면 book/create.html 로 전달
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/create")   // -> localhost:8080/create 접속하면 book/create.html 로 전달
     public String create(){
         return "book/create";
+    }
+
+    @GetMapping("/books")
+    public String list(Model model){
+        List<Book> books = bookService.findBooks();
+        model.addAttribute("books", books);
+        return "book/bookList";
     }
 }
